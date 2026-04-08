97aacd4144c9f6211d861b011a419bb161a37e72
treinartz22 <127342039+treinartz22@users.noreply.github.com>
2026-04-08 13:19:33 -0500

    Add files via upload

---
+++ week12/assignment12/src/components/BookCard.jsx
@@ -0,0 +1,13 @@
+function BookCard({ title, author, image, link, description }) {
+  return (
+    <article className="book-card">
+      <h4>{title}</h4> written by {author}
+      <img src={image} alt={title} className="book-image" />
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        More Here
+      </a>
+      <p>{description}</p>
+    </article>
+  );
+}
+export default BookCard;
---
+++ week12/assignment12/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import BookList from "../components/BookList.jsx";
+
+function Home() {
+  return <BookList />;
+}
+export default Home;
